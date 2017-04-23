cbe2effac3cd3f593ad7e4942c3628430a943449
Alexander <alexander.zybin@gmail.com>
2017-04-23 11:29:35 +0300

    201704231129

--- src/block/site/common/header/_header-menu/.document-ready.js
+++ src/block/site/common/header/_header-menu/.document-ready.js
@@ -5,7 +5,7 @@
 	
 	if(block.length) {
 		
-		var btn = block.find('.header-menu-btn');
+		var _btn = block.find('.header-menu-btn');
 		var _list = block.find('.items._list');
 		var _visible = block.find('.items._visible');
 		var _hidden = block.find('.items._hidden');
@@ -14,8 +14,19 @@
 			event.preventDefault();
 			
 			_hidden.toggleClass('_active');
+			_btn.toggleClass('_active');
 			
 		});
+		
+		$(document.body).on('click.azbn7.header-menu.items.item', '.header-menu .items._hidden._active li, .header-menu .items._hidden._active .item', {}, function(event){
+			event.preventDefault();
+			
+			_hidden.toggleClass('_active');
+			_btn.toggleClass('_active');
+			
+		});
+		
+		//_list.children().clone(true).appendTo(_hidden);
 		
 	}
 	
